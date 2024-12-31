e2e337443394d6968bee10ec8272538e4a09bf62
Denis Kim <denis-kim@outlook.de>
2024-12-31 11:45:33 +0100

    Added tests for PortfolioSection

--- src/tests/PortfolioSection.test.jsx
+++ src/tests/PortfolioSection.test.jsx
@@ -1,14 +1,32 @@
-import { describe, expect, it, vi } from 'vitest';
-import { render, screen } from '@testing-library/react';
+import { describe, expect, it, vi, beforeEach } from 'vitest';
+import { render, screen, waitFor } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import PortfolioSection from '../components/PortfolioSection';  
-import ProjectCard from '../components/ProjectCard'; 
+import CardInfos from '../components/CardInfos'; 
 import ProjectCardsContainer from '../components/ProjectCardsContainer';  
 import { SharedStateProvider } from '../context/SharedStateContext';
 import * as globalConstants from './../globalConstants.js'
 import { testInputs } from './inputs.js';
 
+vi.mock('framer-motion', async () => {
+    const actual = await vi.importActual('framer-motion');
+    return {
+        ...actual,
+        motion: {
+          ...actual.motion,
+          div: vi.fn(({ children, ...props }) => (
+            <div {...props}>{children}</div>
+          )),
+        },
+      };
+  });
 
+  // Mock für PROJECT_CARDS_DATA  
+  beforeEach(() => {
+    vi.spyOn(globalConstants, 'PROJECT_CARDS_DATA', 'get').mockReturnValue(globalConstants.PROJECT_CARDS_DATA);
+  });
+  
+ 
 describe("Tests: PortfolioSection Komponente", () => {
 
     // Hilfsfunktion, um die Komponente mit dem richtigen Provider zu rendern
@@ -17,6 +35,7 @@ describe("Tests: PortfolioSection Komponente", () => {
         <SharedStateProvider initialValue={value}>
             <PortfolioSection />
             <ProjectCardsContainer />
+            <CardInfos />
         </SharedStateProvider>
         );
     };
@@ -38,26 +57,38 @@ describe("Tests: PortfolioSection Komponente", () => {
         expect(portfolioTitle).toHaveTextContent(testInputs.titlePortfolioSection);
     });
 
-    it("TC_02: ProjectCardsContainer wird korrekt angezeigt", () => {
+    it("TC_02: ProjectCardsContainer wird korrekt angezeigt", async () => {
         // Definiere den Zustand des Contexts für den Test
         const initialState = { cardIndex: undefined, isVisible: false };
         renderWithContext(initialState);
 
-        screen.debug();
+        const projectCardsContainer = screen.getByTestId("portfolio-motion-div");
+        expect(projectCardsContainer).toBeInTheDocument();
+        expect(projectCardsContainer).toBeVisible();
 
-        // TBD: Scroll-Animation in PortfolioSection muss noch gemockt werden, um die Karten zu rendern
-
-        // const projectCardsContainer = screen.getAllByTestId("project-cards-container");
-        // projectCardsContainer.forEach(container => {
-        //     expect(container).toBeInTheDocument();
-        // });
-
-        // const projectCards = screen.getAllByTestId("project-card");
-
-        // projectCards.forEach (card => {
-        //     expect(card).toBeInTheDocument();
-        //     expect(card).toBeVisible();
-        // });
+        // Warten, bis die Karten sichtbar sind
+        const projectCards = await waitFor(() =>
+            screen.getAllByTestId("project-card")
+        );
+ 
+        projectCards.forEach (card => {
+            expect(card).toBeInTheDocument();
+            expect(card).toBeVisible();
+        });
     });
 
+    it("TC_03: CardInfos Komponente wird angezeigt, wenn isVisible 'true' ist", async() => {
+        const initialState = { cardIndex: 0, isVisible: true };
+        renderWithContext(initialState);
+
+        const cardInfos = await waitFor(() =>
+            screen.getAllByTestId("card-infos-container")
+        );
+
+        cardInfos.forEach (card => {
+            expect(card).toBeInTheDocument();
+            expect(card).toBeVisible();
+        });
+
+      });
 });
