c0ef766f7e1704683e63669e03151b64f1513f29
Denis Kim <denis-kim@outlook.de>
2025-01-07 18:27:53 +0100

    Added server-wakeup logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
+import { useEffect } from "react";
 import Header from './components/Header';
 import StartSection from './components/StartSection';
 import PortfolioSection from './components/PortfolioSection';
 import AboutSection from './components/AboutSection';
 import ContactSection from './components/ContactSection';
 import Footer from './components/Footer';
+import * as globalConstants from './globalConstants.js';
 import { SharedStateProvider } from './context/SharedStateContext';
 import './styles/App.css'
 
@@ -28,6 +30,23 @@ import './styles/App.css'
  * @see {@link Footer} für die Footer-Komponente.
  */
 function App() {
+
+  useEffect(() => {
+    const wakeUpServer = () => {
+      fetch(`${globalConstants.SERVER_URL}/wakeup`)
+        .then((response) => response.json())
+        .then((data) => console.log(data.message))
+        .catch((error) => console.error(error.message));
+    };
+    // Aufwecken des Servers in Intervallen
+    wakeUpServer();
+
+    // Intervall einrichten (in Millisekunden)
+    const interval = setInterval(wakeUpServer, globalConstants.SERVER_WAKEUP_INTERVAL_min * 60 * 1000);
+
+    // Cleanup-Funktion, um das Intervall zu löschen, wenn die Komponente entladen wird
+    return () => clearInterval(interval);
+  }, []);
 
   return (
     <div className='app-container'>
